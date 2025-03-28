bb820a9d22be1d5fcad1d9008c895ce725583858
Gonzalo <garciacalvog@hotmail.com>
2025-03-28 01:49:55 -0300

    responsive site, animations, dark mode suported

---
+++ src/data/services.js
@@ -0,0 +1,99 @@
+/* export const servicesDetails = {
+	Branding: {
+		id:0,
+		data:["Logo Design", "Visual Indentity", "Brand Book]"]
+	},
+
+	SmallFormat: ["Stationary", "Flyers/Brochures", "Post Cards"],
+	BigFormat: ["Banners", "Roller Ups", "Backdrops", "Wall Art"],
+
+	Infographics: ["Charts and tables", "Iconography", "Animation"],
+	WebDesign: ["Layouts", "Mockups", "Buttons", "Image/video editing"],
+
+	ResponsiveWebdevelopment: ["Front End", "Back End", "Responsive"],
+
+	WebApps: ["Design", "Functionality"],
+
+	SocialMedia: ["Design", "Copyright*", "Community Manager*"],
+
+	Photoediting: ["Rendering", "Photomontage", "Retouching", "AI"],
+
+	VideoEditing: ["Basic Animations", "Audio Effects", "Final Cut"],
+
+	Corporatepresentations: [
+		"Brainstorming",
+		"Plans/Biddings/Reports",
+		"Credentials",
+	],
+
+	VisualMerchandising: [
+		"Packaging",
+		"Display Design",
+		"Furniture design",
+		"E-Commerce*",
+	],
+}; */
+
+export const servicesDetails = {
+	0: {
+			id: "Branding",
+			data: ["Logo Design", "Visual Indentity", "Brand Book"],
+	},
+
+	1: {
+			id: "Small Format",
+			data: ["Stationary", "Flyers/Brochures", "Post Cards"],
+	},
+
+	2: {
+			id: "Big Format",
+			data: ["Banners", "Roller Ups", "Backdrops", "Wall Art"],
+	},
+
+	3: {
+			id: "Infographics",
+			data: ["Charts and tables", "Iconography", "Animation"],
+	},
+
+	4: {
+			id: "Web Design",
+			data: ["Layouts", "Mockups", "Buttons", "Image/video editing"],
+	},
+
+	5: {
+			id: "Responsive Web development",
+			data: ["Front End", "Back End", "Responsive"],
+	},
+
+	6: {
+			id: "Web Apps",
+			data: ["Design", "Functionality"],
+	},
+
+	7: {
+			id: "Social Media",
+			data: ["Design", "Copyright*", "Community Manager*"],
+	},
+
+	8: {
+			id: "Photo editing",
+			data: ["Rendering", "Photomontage", "Retouching", "AI"],
+	},
+
+	9: {
+			id: "Video Editing",
+			data: ["Basic Animations", "Audio Effects", "Final Cut"],
+	},
+
+	10: {
+			id: "Corporate presentations",
+			data: ["Brainstorming", "Plans/Biddings/Reports", "Credentials"],
+	},
+
+	11: {
+			id: "Visual Merchandising",
+			data: ["Packaging", "Display Design", "Furniture design", "E-Commerce*"],
+	},
+};
+
+
